409493d43e5fd9d74949603512c195fd89efb1a2
Tomasz Bilewicz <tomasz.bilewicz@gft.com>
2017-07-24 11:18:42 +0200

    pseudo-pagaobjects

--- test/features/step_definitions/test.steps.js
+++ test/features/step_definitions/test.steps.js
@@ -7,41 +7,46 @@ var expect = chai.expect;
 
 defineSupportCode(function ({ Given, When, Then }) {
 
+    var usernameInput = element(by.id("id1")).$("dl > dd > input[name=\"username\"]");
+    var passwordInput = element(by.id("id1")).$("dl > dd > input[name=\"password\"]");
+    var submitButton = element(by.id("id1")).$("dl > dd > input[name=\"submit\"]");
+    var signOutButton = element(by.xpath("//body//p//a[2]"));
+    var errorMessage = element(by.css(".feedbackPanel > .feedbackPanelERROR"));
+
     Given("I'm on a library's login page", function () {
         browser.get("http://localhost:8080/wicket-examples/library");
     });
 
     When('I enter valid credentials', function () {
-        element(by.id("id1")).$("dl > dd > input[name=\"username\"]").sendKeys("wicket");
-        element(by.id("id1")).$("dl > dd > input[name=\"password\"]").sendKeys("wicket");
-        return element(by.id("id1")).$("dl > dd > input[name=\"submit\"]").click();
+        usernameInput.sendKeys("wicket");
+        passwordInput.sendKeys("wicket");
+        return submitButton.click();
     });
 
     When('I enter \'{username}\' as a username', function (username) {
-        return element(by.id("id1")).$("dl > dd > input[name=\"username\"]").sendKeys(username);
+        return usernameInput.sendKeys(username);
     });
 
     When('I enter \'{password}\' as a password', function (password) {
-        return element(by.id("id1")).$("dl > dd > input[name=\"password\"]").sendKeys(password);
+        return passwordInput.sendKeys(password);
     });
 
     When(`I click "submit" button`, function () {
-        return element(by.id("id1")).$("dl > dd > input[name=\"submit\"]").click();
+        return submitButton.click();
     });
 
-    When('I enter incorrect combination of credentials', function(table) {
-        element(by.id("id1")).$("dl > dd > input[name=\"username\"]").sendKeys(table.rows()[0]);
-        return element(by.id("id1")).$("dl > dd > input[name=\"password\"]").sendKeys(table.rows()[1]);
-    });
+    // Not used for now
+    // When('I enter incorrect combination of credentials', function(table) {
+    //     element(by.id("id1")).$("dl > dd > input[name=\"username\"]").sendKeys(table.rows()[0]);
+    //     return element(by.id("id1")).$("dl > dd > input[name=\"password\"]").sendKeys(table.rows()[1]);
+    // });
 
     Then('I will have access to the library', function () {
-        let signOut = element(by.xpath("//body//p//a[2]"));
-        expect(signOut.isDisplayed()).to.be.eventually.true;
-        return signOut.click();
+        expect(signOutButton.isDisplayed()).to.be.eventually.true;
+        return signOutButton.click();
     });
 
     Then('I will see an error message', function () {
-        let msg = element(by.css(".feedbackPanel > .feedbackPanelERROR"));
-        return expect(msg.getText()).to.eventually.equal("Sign in failed");
+        return expect(errorMessage.getText()).to.eventually.equal("Sign in failed");
     });
 });
